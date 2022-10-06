7f170baaf711f54073fb03e901603fef8e753e5e
Abellybaba <62292802+Abellybaba@users.noreply.github.com>
2022-10-06 10:44:58 -0400

    added products with inside data

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,16 +6,20 @@ import Navbar2 from '../components/shared/Navbar2'
 import Sponsor from '../components/Sponsor'
 import Category from '../components/Category'
 import Shopnow from '../components/Shopnow'
+import Products from '../components/Products'
+//import Product from '../components/Product'
 
 const Home = () => {
   return (
-    <div>
+    <div style={{backgroundColor: "#E8EDF5"}}>
       <Announcement/>
     <Navbar2/>
     <Hero/>
     <Sponsor />
     <Category />
     <Shopnow />
+    <Products/>
+ 
     </div>
   )
 }
