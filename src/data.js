9ea0a08109a367871c8076890371d4542b71b4b5
Abellybaba <62292802+Abellybaba@users.noreply.github.com>
2022-10-29 17:49:08 -0400

    After redux and api

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
     {
       id: 1,
       img: "https://i.ibb.co/XsdmR2c/1.png",
-      title: "SUMMER SALE",
+      title: "SUMMER SALE WEEKEND",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "f5fafd",
     },
